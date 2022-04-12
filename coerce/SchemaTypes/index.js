3afd573984ba9234b77cb54aa06809af5bc8eec6
Pywon <79721537+ThePywon@users.noreply.github.com>
2022-04-12 10:07:22 -0400

    Create index.js

---
+++ coerce/SchemaTypes/index.js
@@ -0,0 +1,7 @@
+// Fetch all default SchemaTypes
+module.exports = {
+  BooleanType: require("./BooleanType"),
+  NumberType: require("./NumberType"),
+  StringType: require("./StringType"),
+  DateType: require("./DateType")
+}
